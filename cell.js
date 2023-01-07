142a635ec2b2db082964546bb2a463aab2b37194
Your Name <you@example.com>
2023-01-08 01:14:36 +0530

    a * algo logic

--- cell.js
+++ cell.js
@@ -4,44 +4,45 @@ function Cell(i, j) {
     this.walls = [true, true, true, true];
     this.visited = false;
 
+    this.f = 0;
+    this.g = 0;
+    this.h = 0;
+
+    this.previous = undefined;
+
+
+    this.neighbors = [];
+
     this.checkNeighbors = function () {
-        var neighbors = [], top, right, left, bottom;
+        let activeNeighour = [];
 
-        if (i < cols - 1) {
-            right = grid[i + 1][j];
-        }
-        if (i > 0) {
-            left = grid[i - 1][j];
-        }
-        if (j < rows - 1) {
-            bottom = grid[i][j + 1];
-        }
-        if (j > 0) {
-            top = grid[i][j - 1];
-        }
+        this.neighbors.forEach(element => {
+            if(!element.visited){
+                activeNeighour.push(element);
+            }
+        });
 
-        if (top && !top.visited) {
-            neighbors.push(top);
-        }
-        if (right && !right.visited) {
-            neighbors.push(right);
-        }
-        if (bottom && !bottom.visited) {
-            neighbors.push(bottom);
-        }
-        if (left && !left.visited) {
-            neighbors.push(left);
-        }
-        if (neighbors.length > 0) {
-            var r = floor(random(0, neighbors.length));
-            return neighbors[r];
+        if (activeNeighour.length > 0) {
+            var r = floor(random(0, activeNeighour.length));
+            return activeNeighour[r];
         } else {
             return undefined;
         }
     }
 
-    this.addNeighbors = function() {
-
+    this.addNeighbors = function () {
+        if (i < cols - 1) {
+            this.neighbors.push(grid[i + 1][j]);
+        }
+        if (i > 0) {
+            this.neighbors.push(grid[i - 1][j]);
+        }
+        if (j < rows - 1) {
+            this.neighbors.push(grid[i][j + 1]);
+        }
+        if (j > 0) {
+            this.neighbors.push(grid[i][j - 1]);
+        }
     }
 
     this.highlight = function (col) {
@@ -50,30 +51,33 @@ function Cell(i, j) {
         noStroke();
         fill(col);
         rect(x, y, w, w);
-
     }
 
-    this.show = function () {
-        var x = this.i * w;
-        var y = this.j * w;
-        stroke(255);
-        if (this.walls[0]) {
-            line(x, y, x + w, y);
-        }
-        if (this.walls[1]) {
-            line(x + w, y, x + w, y + w);
-        }
-        if (this.walls[2]) {
-            line(x + w, y + w, x, y + w);
-        }
-        if (this.walls[3]) {
-            line(x, y + w, x, y);
-        }
-
-        if (this.visited) {
-            noStroke();
-            fill(255, 0, 255, 100);
-            rect(x, y, w, w);
+    this.show = function (col) {
+        if(col){
+            fill(col);
+            rect(this.i * w, this.j * h, w, h);
+        }else{
+            var x = this.i * w;
+            var y = this.j * w;
+            stroke(255);
+            if (this.walls[0]) {
+                line(x, y, x + w, y);
+            }
+            if (this.walls[1]) {
+                line(x + w, y, x + w, y + w);
+            }
+            if (this.walls[2]) {
+                line(x + w, y + w, x, y + w);
+            }
+            if (this.walls[3]) {
+                line(x, y + w, x, y);
+            }
+            if (this.visited) {
+                noStroke();
+                fill(255, 0, 255, 100);
+                rect(x, y, w, w);
+            }
         }
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,16 +1,40 @@
 let w_h = 600;
-let cols = 10;
-let rows = 10;
+let cols = 25;
+let rows = 25;
 let grid = new Array(cols);
 let w, h;
 
-stack = [];
+let stack = [];
 
 let createMaze = true;
+let startAStarAlog = false;
+
+let openSet = [];
+let closedSet = [];
+
+let start;
+let end;
+
+let started = false;
+
+function startMaze() {
+    let temp =  parseInt(document.getElementById("range").value)
+    cols = temp;
+    rows = temp;
+    stack = [];
+    openSet = [];
+    closedSet = []; 
+    started = true;
+    createMaze = true;
+    startAStarAlog = false;
+    this.setup();
+    loop();
+}
 
 
 function setup() {
     createCanvas(w_h, w_h);
+    noLoop();
     w = w_h / cols;
     h = w_h / rows;
     for (var i = 0; i < cols; i++) {
@@ -21,42 +45,143 @@ function setup() {
             grid[i][j] = new Cell(i, j);
         }
     }
-    current = grid[0][0];
-
-}
-
-function draw() {
-    background(51);
 
     for (var i = 0; i < cols; i++) {
         for (var j = 0; j < rows; j++) {
-            grid[i][j].show(color(255, 0, 0, 50));
+            grid[i][j].addNeighbors();
         }
     }
 
-    if(createMaze === true){
-        current.visited = true;
-        current.highlight(color(0, 0, 255, 100));
-    
-        var next = current.checkNeighbors();
-        if (next) {
-            next.visited = true;
-            stack.push(current);
-            removeWalls(current, next);
-            current = next;
-        } else if (stack.length > 0) {
-            current.highlight(color(0, 0, 0, 0));
-            current = stack.pop();
+    current = grid[0][0];
+
+    start = grid[0][0];
+    end = grid[cols - 1][rows - 1];
+
+    openSet.push(start);
+}
+
+function draw() {
+    if (started) {
+        background(51);
+        // frameRate(5);    
+        for (var i = 0; i < cols; i++) {
+            for (var j = 0; j < rows; j++) {
+                grid[i][j].show();
+            }
         }
-    }
-    if(stack.length === 0){
-        current.highlight(color(0, 0, 0, 0));
-        createMaze = false;
-        noLoop();
+        if (createMaze === true) {
+            current.visited = true;
+            current.highlight(color(0, 0, 255, 100));
+            var next = current.checkNeighbors();
+            if (next) {
+                next.visited = true;
+                stack.push(current);
+                removeWalls(current, next);
+                current = next;
+            } else if (stack.length > 0) {
+                current.highlight(color(0, 0, 0, 0));
+                current = stack.pop();
+            }
+        }
+        if (stack.length === 0 && createMaze === true) {
+            console.log("Maze completed!");
+            current.highlight(color(0, 0, 0, 0));
+            createMaze = false;
+            startAStarAlog = true;
+        }
+        if (startAStarAlog === true) {
+            if (openSet.length > 0) {
+                var winner = 0;
+                for (var i = 0; i < openSet.length; i++) {
+                    if (openSet[i].f < openSet[winner].f) {
+                        winner = i;
+                    }
+                }
+                current = openSet[winner];
+                if (current === end) {
+                    noLoop();
+                    console.log("DONE!");
+                }
+                removeFromArray(openSet, current);
+                closedSet.push(current);
+                var neighbors = current.neighbors;
+                for (var i = 0; i < neighbors.length; i++) {
+                    var neighbor = neighbors[i];
+                    if (!closedSet.includes(neighbor) && !this.current.walls[this.getWallIndex(current, neighbor)]) {
+                        var tempG = current.g + heuristic(neighbor, current);
+                        var newPath = false;
+                        if (openSet.includes(neighbor)) {
+                            if (tempG < neighbor.g) {
+                                neighbor.g = tempG;
+                                newPath = true;
+                            }
+                        } else {
+                            neighbor.g = tempG;
+                            newPath = true;
+                            openSet.push(neighbor);
+                        }
+                        if (newPath) {
+                            neighbor.h = heuristic(neighbor, end);
+                            neighbor.f = neighbor.g + neighbor.h;
+                            neighbor.previous = current;
+                        }
+                    }
+
+                }
+            } else {
+                console.log('no solution');
+                noLoop();
+                return;
+            }
+
+            for (var i = 0; i < cols; i++) {
+                for (var j = 0; j < rows; j++) {
+                    grid[i][j].show();
+                }
+            }
+
+            for (var i = 0; i < closedSet.length; i++) {
+                closedSet[i].show(color(255, 0, 0, 60));
+            }
+
+            for (var i = 0; i < openSet.length; i++) {
+                openSet[i].show(color(0, 255, 50, 50));
+            }
+
+            path = [];
+            var temp = current;
+            path.push(temp);
+            while (temp.previous) {
+                path.push(temp.previous);
+                temp = temp.previous;
+            }
+
+            noFill();
+            stroke(26, 44, 51);
+            beginShape();
+            for (var i = 0; i < path.length; i++) {
+                vertex(path[i].i * w + w / 2, path[i].j * h + h / 2);
+            }
+            endShape();
+        }
     }
 }
 
-
+function getWallIndex(a, b) {
+    if (a.i - b.i === -1) {
+        return 1;
+    }
+    if (a.j - b.j === 1) {
+        return 0;
+    }
+    if (a.i - b.i === 1) {
+        return 3;
+    }
+    if (a.j - b.j === -1) {
+        return 2;
+    }
+    return ""
+}
 
 function removeWalls(a, b) {
     var x = a.i - b.i;
@@ -75,4 +200,18 @@ function removeWalls(a, b) {
         a.walls[2] = false;
         b.walls[0] = false;
     }
+}
+
+function removeFromArray(arr, elt) {
+    for (var i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] == elt) {
+            arr.splice(i, 1);
+        }
+    }
+}
+
+function heuristic(a, b) {
+    var d = dist(a.i, a.j, b.i, b.j);
+    // var d = abs(a.i - b.i) + abs(a.j - b.j);
+    return d;
 }
